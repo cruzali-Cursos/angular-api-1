161ca1957a1018d49fdbab246401089b577439f9
ali.cruz <ali.cruz@me.com>
2024-01-25 17:32:01 -0600

    update

--- src/app/pages/gestion/gestion-routing.module.ts
+++ src/app/pages/gestion/gestion-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AltasComponent } from './altas/altas.component';
-import { GestionComponent } from './gestion.component';
 
 const routes: Routes = [
-  { path: 'gestion', component: GestionComponent },
+  
   { path: 'altas', component: AltasComponent }
 ];
 
